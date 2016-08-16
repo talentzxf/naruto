feb5bbb03c2a005d77a34fe93a73d725b1e1e613
Zhang <vincentzhang@ea.com>
2016-08-16 17:16:15 +0800

    Added some matrix functions

--- src/Chakra/Algebra/Matrix.js
+++ src/Chakra/Algebra/Matrix.js
@@ -1,23 +1,30 @@
 'use strict'
 class Matrix{
-    constructor(inArray){
-        // TODO: Validate this array is 2d array
-        this.data = inArray;
-        this.rows = inArray.length;
-        this.cols = inArray[0].length;
-    }
+    constructor(){
+        if(arguments.length == 1){
+            if(arguments[0] instanceof Array){
+                var inArray = arguments[0];
+                if(!(inArray[0] instanceof Array)){
+                    assert("Input array not 2d.")
+                }
+                // TODO: Validate this array is 2d array
+                this.data = inArray;
+                this.rows = inArray.length;
 
-    //constructor(rows, cols){
-    //    this.data = []
-    //    this.rows = rows;
-    //    this.cols = cols;
-    //    for(var r = 0 ; r < rows; r++){
-    //        this.data
-    //        for( var c = 0 ; c < cols; c++){
-    //
-    //        }
-    //    }
-    //}
+                this.cols = inArray[0].length;
+            }
+        }else if(arguments.length == 2){
+            this.data = []
+            this.rows = arguments[0];
+            this.cols = arguments[1];
+            for(var r = 0 ; r < this.rows; r++){
+                this.data.push([]);
+                for( var c = 0 ; c < this.cols; c++){
+                    this.data[r].push(0);
+                }
+            }
+        }
+    }
 
     getEle(r,c){
         return this.data[r][c];
@@ -27,21 +34,77 @@ class Matrix{
         this.data[r][c] = v;
     }
 
-    //add(m){
-    //    if(!m instanceof Matrix)
-    //        assert(m + "is not a matrix!")
-    //
-    //    if(m.getCols() != this.getCols() || m.getRows() != this.getRows()){
-    //        assert("Matrix dimension didn't match!")
-    //    }
-    //
-    //    let m = new Matrix();
-    //    for()
-    //}
-    //
-    //getCols(){
-    //        return
-    //    }
+    /**
+     * Matrix-Matrix addition.
+     * @param m
+     */
+    add(m){
+        if(!m instanceof Matrix)
+            assert(m + "is not a matrix!")
+
+        if(m.rows != this.rows || m.cols != this.cols){
+            assert("Matrix dimension didn't match!")
+        }
+
+        let retM = new Matrix(m.rows, m.cols);
+        for(var r = 0; r < m.rows; r++){
+            for(var c = 0 ; c < m.rows; c++){
+                retM.setEle(r,c, m.getEle(r,c) + this.getEle(r,c))
+            }
+        }
+        return retM;
+    }
+
+    /**
+     * Transpose a matrix
+     * @constructor
+     */
+    T(){
+        var retM = new Matrix(this.cols, this.rows);
+        for(var r = 0; r < this.rows; r++){
+            for(var c = 0 ; c < this.cols; c++){
+                retM.setEle(c,r, this.getEle(r,c));
+            }
+        }
+        return retM;
+    }
+
+    /**
+     * Matrix multiply
+     * @returns {string}
+     */
+    multi(){
+        if(arguments.length == 1){
+            if(!(arguments[0] instanceof Matrix )){ // Number multiply
+                var retM = new Matrix(this.rows, this.cols);
+                var num = arguments[0]
+                for(var r = 0; r < this.rows; r++) {
+                    for (var c = 0; c < this.cols; c++) {
+                        retM.setEle(r, c, this.getEle(r, c) * num);
+                    }
+                }
+                return retM;
+            }else{   //Matrix multiply matrix
+                var m = arguments[0]
+                if(this.cols == m.rows){
+                    var retM = new Matrix(this.rows, m.cols);
+                    for(var r = 0; r < this.rows; r++){
+                        for(var c = 0 ; c < m.cols; c++){
+                            var res = 0;
+                            for(var index = 0; index < this.cols; index++){
+                                res += this.getEle(r, index) * m.getEle(index, c);
+                            }
+                            retM.setEle(r,c, res);
+                        }
+                    }
+                    return retM;
+                }
+                else{
+                    assert("Matrix dimension not match!Left col:" + this.cols + " right rows:" + m.rows);
+                }
+            }
+        }
+    }
 
     toString(){
         var retStr = "";
@@ -51,6 +114,8 @@ class Matrix{
             }
             retStr += "\n";
         }
+
+        return retStr;
     }
 }
 
--- src/Chakra/test/test.js
+++ src/Chakra/test/test.js
@@ -3,13 +3,72 @@ import {Matrix} from '../Algebra/Matrix.js';
 
 var assert = require('assert');
 describe('Matrix', function() {
-    describe('#set/get', function() {
+    describe('#constructors', function() {
         it('Element should be the same as provided', function() {
-            let mat = new Matrix([[1,2],[2,3]])
-            assert.equal(1,mat.getEle(0,0));
+            let mat1 = new Matrix([[1,2],[2,3]])
+            assert.equal(1,mat1.getEle(0,0));
 
-            console.log(mat)
+            let mat2 = new Matrix(5,3)
+            assert.equal(0,mat2.getEle(4,2));
         });
     });
+
+    describe('#add', function() {
+        it('Matrix addition', function() {
+            let mat1 = new Matrix([[1,2],[2,3]])
+            console.log("Matrix is:\n" + mat1.toString())
+            assert.equal(1,mat1.getEle(0,0));
+
+            let mat2 = mat1.add(new Matrix(2,2))
+            console.log("Matrix is:\n" + mat2.toString())
+            assert.equal(3,mat2.getEle(1,1));
+
+            let mat3 = mat1.add(new Matrix([[2,3],[4,5]]))
+            console.log("Matrix is:\n" + mat3)
+            assert.equal(3,mat3.getEle(0,0));
+            assert.equal(5,mat3.getEle(0,1));
+            assert.equal(6,mat3.getEle(1,0));
+            assert.equal(8,mat3.getEle(1,1));
+        });
+    });
+
+    describe('#transpose', function() {
+        it('Matrix transpose', function() {
+            let mat = (new Matrix([[1,2]])).T();
+            console.log("Mat:" + mat);
+            assert.equal(1,mat.getEle(0,0))
+            assert.equal(2,mat.getEle(1,0))
+
+            let mat1 = (new Matrix([[1,2],[3,4],[5,6]])).T();
+            assert.equal(1,mat1.getEle(0,0));
+            assert.equal(3,mat1.getEle(0,1));
+            assert.equal(5,mat1.getEle(0,2));
+            assert.equal(2,mat1.getEle(1,0));
+            assert.equal(4,mat1.getEle(1,1));
+            assert.equal(6,mat1.getEle(1,2));
+        });
+    });
+
+    describe("#multiply", function(){
+        it("num multiply",function(){
+            let mat = new Matrix([[1,2,3,4,5]]).multi(3);
+            assert.equal(3,mat.getEle(0,0))
+            assert.equal(6,mat.getEle(0,1))
+            assert.equal(9,mat.getEle(0,2))
+            assert.equal(12,mat.getEle(0,3))
+            assert.equal(15,mat.getEle(0,4))
+        })
+
+        it("Matrix multiply matrix", function(){
+            let mat1 = new Matrix([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+            let mat2 = new Matrix([[13,14,15,16],[17,18,19,20],[21,22,23,24]])
+            let mat3 = mat1.multi(mat2)
+            console.log(mat3)
+            assert.equal(1*13+2*17+3*21, mat3.getEle(0,0))
+            assert.equal(1*14+2*18+3*22, mat3.getEle(0,1))
+            assert.equal(4*13+5*17+6*21, mat3.getEle(1,0))
+            assert.equal(10*16+11*20+12*24, mat3.getEle(3,3))
+        })
+    })
 });
 
