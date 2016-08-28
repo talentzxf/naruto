7b0e2b7761387658efdbca671bbdac24d5085023
Zhang <vincentzhang@ea.com>
2016-08-28 10:45:23 +0800

    First version of Force system simplify

--- src/Chakra/Mechanics/Force.js
+++ src/Chakra/Mechanics/Force.js
@@ -4,9 +4,9 @@ import {Torque} from './Torque.js';
 // A force contains two factors
 // 1. A free vector indicates magnitude and direction of force
 // 2. Point of action
-class Force{
+class Force extends Vector{
     constructor(){
-        if( arguments.length != 2 )
+        if( arguments.length != 1 || arguments.length != 2 )
             throw "Pleaes input two vectors, first one is force direction/magnitude. Second is point of action. Current argument length" + arguments.length
 
         if(arguments[0] instanceof Vector && arguments[0].rows == 3){
@@ -41,6 +41,39 @@ class ForceSystem{
         }
         this.forces.push(f);
         this.mainMoment.iadd(f.force);
+    }
+
+    // Simplify the force system.
+    // May encounter three types of results.
+    // 0. Force balance. i.e. mainMoment=0 and mainTorque is zero.
+    // 1. Force couple
+    // 2. Joint force
+    // 3. Force screw
+    simplify(){
+        // Randomly pick a point to test.
+        let testPoint = new Vector([0,0,0]);
+        var simplifiedRes = simplifyTo(testPoint);
+        if(this.mainMoment.norm2() == 0){
+            // Force == 0 , Torque == 0. Balance
+            if(simplifiedRes.T.norm2() == 0){
+                return {status:0}
+            } else {
+                // Force == 0 , Torque == 0. Equal to a Force dual.
+                return {status:1, torque:simplifiedRes.T}
+            }
+        }
+
+        // Force <> 0, Torque == 0. Can be simplified as a Joint force to several points.
+        if(simplifiedRes.T.getTorque().norm2() == 0){
+            return {status:2, force: new Force( this.mainMoment , testPoint )};
+        }else{
+            // The most complicated case. Force <>0 and Torque<>0
+            // Calculate the invariant variable T dot F.
+            var TdF = simplifiedRes.T.getTorque().dot(this.mainMoment);
+            if(TdF == 0 ){
+
+            }
+        }
     }
 
     simplifyTo(p){
