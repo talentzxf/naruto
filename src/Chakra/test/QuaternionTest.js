f96176d19f5866b672f418406dcbb9651568d1af
Zhang <vincentzhang@ea.com>
2016-08-28 14:05:40 +0800

    First commit of Quaternion.

---
+++ src/Chakra/test/QuaternionTest.js
@@ -0,0 +1,9 @@
+'use strict'
+import {Quaternion} from '../Algebra/Quaternion.js';
+
+var assert = require('assert');
+describe('Quaternion', function() {
+   describe("#constructor",function(){
+       
+   });
+});
